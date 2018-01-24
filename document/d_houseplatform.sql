81bdbf6c7732d6282c6e055ed6fef09acc5f44d0
Yaolz <478095694@qq.com>
2018-01-24 09:11:53 +0800

    修改

--- document/d_houseplatform.sql
+++ document/d_houseplatform.sql
@@ -1,18 +1,3 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : sa
-Source Server Version : 50717
-Source Host           : localhost:3306
-Source Database       : d_houseplatform
-
-Target Server Type    : MYSQL
-Target Server Version : 50717
-File Encoding         : 65001
-
-Date: 2018-01-24 08:51:46
-*/
-
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
